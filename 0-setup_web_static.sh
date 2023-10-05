c7a6712cbfe6b00efec3a480b9484d33a2c5f267
Julesntizimira <ntizimijules5@gmail.com>
2023-10-05 14:46:52 +0000

    correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+# Bash script that sets up your web servers for the deployment of web_static.
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo sh -c 'echo "simple content, to test your Nginx configuration" > /data/web_static/releases/test/index.html'
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name _;/ a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo 'conext' > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+old="server_name _;"
+new="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "s|$old|$new|" /etc/nginx/sites-enabled/default
 sudo service nginx restart
