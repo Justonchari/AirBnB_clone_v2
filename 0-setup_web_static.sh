b2c95ffa0f8da8e574a907838744eb5131ee9703
Julesntizimira <ntizimijules5@gmail.com>
2023-10-08 07:30:30 +0000

    revise task1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
 sudo apt-get update
-sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo 'conext' > /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo apt-get install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo sh -c 'echo "simple content" > /data/web_static/releases/test/index.html'
+sudo ln -sfn /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 old="server_name _;"
 new="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "s|$old|$new|" /etc/nginx/sites-enabled/default
